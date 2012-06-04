376b5eebd0d31e0446ba9fc15460580b0e4e9f0c
Omar Erminy <omrsin@gmail.com>
2012-06-04 12:12:47 -0430

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cleteci::Application.config.secret_token = 'ec7395eec1dee789adcca0e5b9f65ea9785eaedca8888650f848ed57b00fe3f74822d972948637189fa14a4296b66cb3c058ba076a4bd47ac68d85932209906d'
