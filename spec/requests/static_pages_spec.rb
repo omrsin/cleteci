a87d821063a10f2f4f73ff5179a25f0d610ebf7f
Omar Erminy <omrsin@gmail.com>
2012-06-04 18:10:39 -0430

    Ruta base apuntando a Home

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,7 @@ describe "Static pages" do
 	subject { page }
 
   describe "Home page" do
-  	before { visit '/static_pages/home' }
+  	before { visit root_path }
     it { should have_selector('title', text: "CLETECI") }
   end
 end
