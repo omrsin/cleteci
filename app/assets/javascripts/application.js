70460134b4ba9617969c6ec40e334d546bf018db
Omar Erminy <omrsin@gmail.com>
2012-06-06 16:09:13 -0430

    Incluye estilos de bootstrap a modo de prueba

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-carousel
+//= require bootstrap-transition
 //= require_tree .
+
+$(function(){	
+	// carousel demo
+  $('.carousel').carousel();
+});
