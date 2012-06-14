702e2f40cd903e90162d8464e2ec6c7c57f2a82f
Omar Erminy <omrsin@gmail.com>
2012-06-13 19:46:46 -0430

    Carusel con funcionalidad swipe touch para dispositivos touchscreen

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,55 @@
 //= require_tree .
 
 $(function(){	
-	// carousel demo
-  $('.carousel').carousel();
+  $('.carousel').carousel(); 
+  
+});
+
+$(document).ready(function(){
+	var maxTime = 1000,
+			target = $('.carousel'),
+			startX = 0,
+    	startTime = 0,
+    	touch = "ontouchend" in document,
+    	startEvent = 'touchstart',
+    	moveEvent = 'touchmove',
+    	endEvent = 'touchend';
+    	
+  target
+  	.bind(startEvent, function(e){
+  		e.preventDefault();
+      startTime = e.timeStamp;
+      startX = e.originalEvent.touches ? e.originalEvent.touches[0].pageX : e.pageX;
+      target.carousel('pause');
+  	})
+		.bind(endEvent, function(e){
+			startTime = 0;
+      startX = 0;
+      target.carousel('cycle')
+		})
+		.bind(moveEvent, function(e){
+        e.preventDefault();
+        var currentX = e.originalEvent.touches ? e.originalEvent.touches[0].pageX : e.pageX,
+            currentDistance = (startX === 0) ? 0 : Math.abs(currentX - startX),
+            // allow if movement < 1 sec
+            currentTime = e.timeStamp;
+        if (startTime !== 0 && currentTime - startTime < maxTime) {
+            if (currentX < startX) {
+                // swipe left code here
+                target.carousel('next');
+            }
+            if (currentX > startX) {
+                // swipe right code here
+                 target.carousel('prev');
+            }
+            startTime = 0;
+            startX = 0;
+        }
+    });
+		
+		
+		
+		
+		
+		
 });
