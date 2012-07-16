2c91554066c7766d936268db701b3fd88374be55
Omar Erminy <omrsin@gmail.com>
2012-07-16 14:44:00 -0430

    Con estructura para internacionalización

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,16 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
   include SessionsHelper
   
   private
 
     def logged_in_user
       redirect_to root_path, notice: "Dirección inválida" unless logged_in?
+    end
+    
+    def set_locale
+    	I18n.locale = "es"
     end
 end
